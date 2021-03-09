2b2102c4d41bacd3473e664cd9632298741042b1
Linard Jeremy <linard.jeremy.5@gmail.com>
2021-03-09 14:05:44 +0100

    t

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:LinardJeremy/news_app.git master:gh-pages
+git push -f git@github.com:LinardJeremy/news_app.git 
 cd -
